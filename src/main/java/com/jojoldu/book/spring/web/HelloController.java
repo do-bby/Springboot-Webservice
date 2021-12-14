6cb5c2f7f397283550f3105dc59010aed2f725d2
do-bby <qkrdbstn25@naver.com>
2021-12-14 17:45:50 +0900

    단위테스트 및 lombok설정

---
+++ src/main/java/com/jojoldu/book/spring/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.jojoldu.book.spring.web;
+import com.jojoldu.book.spring.web.Dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        //@RequestParam : 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+        //외부에서 name(@RequestParam("name))이란 이름으로 넘긴 파라미터를 메소드 파라미터 name(String name)에 저장
+        return new HelloResponseDto(name, amount);
+    }
+}
---
+++ src/test/java/com/jojoldu/book/spring/web/HelloControllerTest.java
@@ -0,0 +1,48 @@
+package com.jojoldu.book.spring.web;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
+@RunWith(SpringRunner.class) // 테스트를 진행할 떄 Junit에 내장된 실행자 외에 다른 실행자 실행, 스프링 부트 테스트와 Junit사이에 연결자 역할
+@WebMvcTest(controllers = HelloController.class)
+// 여러 스프링 테스트 어노테이션 중 , web에 집중할 수 있는 어노테이션
+public class HelloControllerTest {
+
+    @Autowired // 스프링이 관리하는 빈을 주입 받습니다.
+    private MockMvc mvc; // 웹 API를 테스트할 떄 사용, 스프링 MVC테스트 시작점
+
+    @Test
+    public void hello가리턴된다() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello")) //MockMvc를 통해 /hello주소로 get요청
+                .andExpect(status().isOk()) // mvc.perform의 결과 검증(status 검증 ex) 200,400)
+                .andExpect(content().string(hello)); // mvc.perform의 결과 검증(응답 본문의 내용 검증)
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk()).andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+
+    //param : API테스트할 떄 사용될 요청 파라미터를 설정(값은 String만 허용)
+    //jsonPath : JSON응답갓ㅂ을 필드별로 검증할 수 있는 메소드($를 기준으로 필드명 명시)
+
+
+}

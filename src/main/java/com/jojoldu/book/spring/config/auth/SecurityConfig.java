156920360d92619e804ccc9fa6bc08ac4d64d61c
do-bby <qkrdbstn25@naver.com>
2021-12-17 20:16:50 +0900

    스프링 구글 로그인,로그아웃,회원가입,권한관리

---
+++ src/main/java/com/jojoldu/book/spring/config/auth/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.jojoldu.book.spring.config.auth;
+
+import com.jojoldu.book.spring.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+//Spring Security 설정들을 활성화시켜 줍니다.
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf()
+                .disable()
+                .headers()
+                .frameOptions()
+                .disable()
+                .and().authorizeRequests().antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated().and().logout().logoutSuccessUrl("/")
+                .and().oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+
+        // csrf().disable().headers().frameOptions().disable() : h2-console화면을 사용하기 위해 해당 옵션 disable
+        // authorizeRequests : URL별 권한 권리 설정하는 옵션 시작점(authorizeRequests선언 되야 andMatchers옵션 사용가능)
+        // andMatchers : 권한 관리 대상 지정하는 옵션( URL, HTTP 메소드별 관리 가능)
+        // "/" 지정 URL들은 permitAll()옵션을 통해 전체 열람 권한, "/api/v1/**"주소를 가진 API는 USER권한 가진 사람만 가능
+        // anyRequest : 설정된 값들 이외 나머지 URL들을 나타냄
+        // logout().logoutSuccessUrl("/") : 로그아웃 기능에 대한 여러 설정 진입점(로그아웃 성공시 /주소로 이동)
+        // oauth2Login : OAuth2 로그인 기능에 대한 여러 설정 진입점
+        // userInfoEndpoint : OAuth2 로그인 성공 이후 사용자 정보 가져올떄의 설정 담당
+        // userService : 소셜 로그인 성공 시 후속 조치 진행할 UserService인터페이스 구현체 등록
+
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.httpFirewall(defaultHttpFirewall());
+    }
+    @Bean
+    public HttpFirewall defaultHttpFirewall() {
+        return new DefaultHttpFirewall();
+    }
+}
--- src/main/java/com/jojoldu/book/spring/web/IndexController.java
+++ src/main/java/com/jojoldu/book/spring/web/IndexController.java
@@ -1,4 +1,5 @@
 package com.jojoldu.book.spring.web;
+import com.jojoldu.book.spring.config.auth.dto.SessionUser;
 import com.jojoldu.book.spring.service.posts.PostsService;
 import com.jojoldu.book.spring.web.Dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -6,16 +7,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        //(SessionUser) httpSession.getAttribute("user") : 로그인 성공시 httpSession.getAttribute("user")에서 값을 가져올 수 있다.
+        if (user != null){
+            model.addAttribute("userName", user.getName());
+        }
+        // 세션에 저장된 값이 있을 때만 model이 userName으로 등록, 값이 없으면 model엔 아무값도 없으므로 로그인 버튼이 보이게 된다.
         return "index";
     }
     //Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
